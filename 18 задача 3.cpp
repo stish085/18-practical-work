9af2d91f7633d25d89d8d4d45a98f1e2dcea9a81
bestCoder <127156447+stish085@users.noreply.github.com>
2025-07-24 02:21:13 +0300

    Add files via upload

---
+++ 18 задача 3.cpp
@@ -0,0 +1,27 @@
+﻿// 18 задача 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+void evenNumbers(long long n,int&ans)
+{ 
+    int lastNum;
+    if (n==0)
+    {
+        return;
+    }
+    lastNum = n % 10;
+    if (lastNum%2==0)
+    {
+        ans++;
+    } 
+    evenNumbers(n / 10, ans);
+}
+int main()
+{
+    long long n= 9223372036854775806;
+    std::cout << "Col even numbers in number: "<<n<<" :"<<std::endl;
+    int ans=0; 
+    evenNumbers(n,ans);
+    std::cout << ans;
+}
+
